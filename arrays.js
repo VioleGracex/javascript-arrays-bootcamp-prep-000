688e6905908ea4c66fefc0f13b8fa870dddd86d0
highdevilxx@hotmail.com Khaled <highdevilxx@hotmail.com>
2018-08-08 03:15:49 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,5 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray(chocolateBars, candyString)
+{ //chocolateBars =["candyString","snickers", "hundred grand", "kitkat", "skittles"];
+  chocolateBars.unshift(candyString);
+}
