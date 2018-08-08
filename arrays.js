3c0a45a886a67a83a29115392235ac93f79bee82
highdevilxx@hotmail.com Khaled <highdevilxx@hotmail.com>
2018-08-08 03:30:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var candyString = "foo"
 function addElementToBeginningOfArray(chocolateBars, candyString)
-{ //chocolateBars =["candyString","snickers", "hundred grand", "kitkat", "skittles"];
+{
   chocolateBars.unshift(candyString);
+}
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString)
+{
+  chocolateBars.shift();
+  chocolateBars.unshift("foo");
 }
